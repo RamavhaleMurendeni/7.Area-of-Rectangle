1643e7b4db852f0196dfaa670e8258b5e73a1dca
Ramavhale Murendeni <Ramavhale Murendeni@DESKTOP-CO1SVRB>
2024-01-18 22:21:56 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(158, 85)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(74, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter Length"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(158, 125)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(73, 15)
+        Label2.TabIndex = 0
+        Label2.Text = "Enter Height"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button1.Location = New Point(148, 158)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(83, 39)
+        Button1.TabIndex = 1
+        Button1.Text = "Area Is"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button2.Location = New Point(149, 215)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(82, 41)
+        Button2.TabIndex = 1
+        Button2.Text = "Perimeter is"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.ScrollBar
+        TextBox1.Location = New Point(309, 84)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(181, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = SystemColors.ScrollBar
+        TextBox2.Location = New Point(309, 125)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(181, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BackColor = SystemColors.ScrollBar
+        TextBox3.Location = New Point(309, 167)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(181, 23)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BackColor = SystemColors.ScrollBar
+        TextBox4.Location = New Point(309, 215)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(181, 23)
+        TextBox4.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightCoral
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Area of a Rectangle"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+End Class
